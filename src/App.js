e7693e69b59a1416f55a7cc7440ce0a5b41a5ea5
Amélie <ameliechevalier3006@gmail.com>
2020-04-13 14:23:52 +0200

    import logo and add title import font work on CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './App.css'
+import logoFull from './images/logoFull.svg';
+
+console.log(logoFull);
 
 function App () {
   return (
     <div className='App'>
-      <h1>Movie Match</h1>
+      <img src={logoFull} alt='full logo' className='logo' />
+      <h1>TROUVER UN FILM A REGARDER CE SOIR !</h1>
     </div>
   );
 }

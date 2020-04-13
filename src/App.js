e35b0eab9d07adc1e1696f3ece81c6547ea34e2f
Amélie <ameliechevalier3006@gmail.com>
2020-04-13 13:44:01 +0200

    cleaning App css and js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
 
-function App() {
+function App () {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Movie Match</h1>
     </div>
   );

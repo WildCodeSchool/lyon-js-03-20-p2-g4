99e59622305ef054ed92d4ce79dd8db10fd3ffdf
tmicka23 <mickathp42@gmail.com>
2020-04-15 10:49:17 +0200

    create folders for components & styles

---
+++ src/components/UsersInputs.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "../styles/UsersInputs.css";
+import Button from "./Button";
+
+class UsersInputs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user1: "Tic",
+      user2: "Tac",
+    };
+  }
+
+  handleChange = (event) => {
+    const user = event.target.value;
+    this.setState({ user1: user });
+  };
+
+  handleChange2 = (event) => {
+    const user = event.target.value;
+    this.setState({ user2: user });
+  };
+
+  works = () => {
+    alert("it works ! write the method/function you need !");
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h2 className="subtitle">Qui participe ?</h2>
+        <div className="form-group">
+          <label htmlFor="user1"></label>
+          <input
+            className="content"
+            type="text"
+            id="user1"
+            name="user1"
+            placeholder="Utilisateur 1"
+            defaultValue={this.state.user1}
+            onChange={this.handleChange1}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="user2"></label>
+          <input
+            className="content"
+            type="text"
+            id="user2"
+            name="user2"
+            placeholder="Utilisateur 2"
+            defaultValue={this.state.user2}
+            onChange={this.handleChange2}
+          />
+        </div>
+        <Button txt="Valider" func={this.works} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default UsersInputs;

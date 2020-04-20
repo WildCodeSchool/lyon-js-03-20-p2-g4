6304dfce37fabef26718d43b2154d053bc9a1818
Amélie <ameliechevalier3006@gmail.com>
2020-04-20 17:31:24 +0200

    superposition of three cards with css

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -6,16 +6,22 @@ class FilmCard extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      index: 0
+      index: 1
     };
   }
 
   render () {
     const film = listAction.results[this.state.index];
     const posterPath = listAction.results[this.state.index].poster_path;
+    const posterPath2 = listAction.results[this.state.index + 1].poster_path;
+    const posterPath3 = listAction.results[this.state.index + 2].poster_path;
     return (
-      <div key={film.id} className='film-card-container' style={{ backgroundImage: `url(http://image.tmdb.org/t/p/w342/${posterPath})` }} />
-    )
+      <div className='all-films-cards'>
+        <div key={film.id} className='film-card-container' style={{ backgroundImage: `url(http://image.tmdb.org/t/p/w342/${posterPath})` }} />
+        <div key={film.id} className='film-card-container' style={{ backgroundImage: `url(http://image.tmdb.org/t/p/w342/${posterPath2})` }} />  
+        <div key={film.id} className='film-card-container' style={{ backgroundImage: `url(http://image.tmdb.org/t/p/w342/${posterPath3})` }} />
+      </div>
+    );
   }
 }
 

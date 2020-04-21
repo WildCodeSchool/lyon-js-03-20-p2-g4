55a7de5153ab5a19ecc12b64b71aa9cd03ae380c
Amélie <ameliechevalier3006@gmail.com>
2020-04-21 10:29:21 +0200

    add 'A vous user 1'

--- src/components/User1List.js
+++ src/components/User1List.js
@@ -4,7 +4,10 @@ import '../styles/UserList.css';
 
 function User1List (props) {
   return (
-    <div>
+    <div className='user-list-container'>
+      <div className='pop-up-user-session'>
+        <h2 className='subtitle'>{`À vous ${props.user1}`}</h2>
+      </div>
       <h2 className='user-session'>Utilisateur : {props.user1}</h2>
       <FilmCard />
     </div>

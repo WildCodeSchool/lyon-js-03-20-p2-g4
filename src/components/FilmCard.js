5799d18de0ef9a37271c1b442cc4c8d725512966
tmicka23 <mickathp42@gmail.com>
2020-04-27 10:00:30 +0200

    fix undefined in FilmCard

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import listAction from './listAction.json';
-import '../styles/FilmCard.css';
+import React from "react";
+import listAction from "./listAction.json";
+import "../styles/FilmCard.css";
 
 class FilmCard extends React.Component {
-  render () {
-    const posterPath = listAction.results[this.props.index].poster_path;
-    const posterPath2 = listAction.results[this.props.index + 1].poster_path;
-    const posterPath3 = listAction.results[this.props.index + 2].poster_path;
+  render() {
+    const posterPath = listAction.results[this.props.index] ? (
+      <div
+        key={listAction.results[this.props.index].poster_path}
+        className="film-card-container"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${
+            listAction.results[this.props.index].poster_path
+          })`,
+        }}
+      />
+    ) : (
+      <div></div>
+    );
+    const posterPath2 = listAction.results[this.props.index + 1] ? (
+      <div
+        key={listAction.results[this.props.index + 1].poster_path}
+        className="film-card-container"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${
+            listAction.results[this.props.index + 1].poster_path
+          })`,
+        }}
+      />
+    ) : (
+      <div></div>
+    );
+    const posterPath3 = listAction.results[this.props.index + 2] ? (
+      <div
+        key={listAction.results[this.props.index + 2].poster_path}
+        className="film-card-container"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${
+            listAction.results[this.props.index + 2].poster_path
+          })`,
+        }}
+      />
+    ) : (
+      <div></div>
+    );
     return (
-      <div className='all-films-cards'>
-        <div key={posterPath} className='film-card-container' style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${posterPath})` }} />
-        <div key={posterPath2} className='film-card-container' style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${posterPath2})` }} />
-        <div key={posterPath3} className='film-card-container' style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${posterPath3})` }} />
+      <div className="all-films-cards">
+        {posterPath}
+        {posterPath2}
+        {posterPath3}
       </div>
     );
   }

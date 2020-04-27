0031e89a6b811b7b17814411c4e2414441de8ed1
tmicka23 <mickathp42@gmail.com>
2020-04-27 10:05:21 +0200

    refactor ternaire statement & style empty-card

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -1,50 +1,48 @@
-import React from "react";
-import listAction from "./listAction.json";
-import "../styles/FilmCard.css";
+import React from 'react';
+import listAction from './listAction.json';
+import '../styles/FilmCard.css';
 
 class FilmCard extends React.Component {
-  render() {
-    const posterPath = listAction.results[this.props.index] ? (
+  render () {
+    const posterPath = (
       <div
         key={listAction.results[this.props.index].poster_path}
-        className="film-card-container"
+        className='film-card-container'
         style={{
           backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${
             listAction.results[this.props.index].poster_path
-          })`,
+          })`
         }}
       />
-    ) : (
-      <div></div>
     );
     const posterPath2 = listAction.results[this.props.index + 1] ? (
       <div
         key={listAction.results[this.props.index + 1].poster_path}
-        className="film-card-container"
+        className='film-card-container'
         style={{
           backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${
             listAction.results[this.props.index + 1].poster_path
-          })`,
+          })`
         }}
       />
     ) : (
-      <div></div>
+      <div className='empty-card' />
     );
     const posterPath3 = listAction.results[this.props.index + 2] ? (
       <div
         key={listAction.results[this.props.index + 2].poster_path}
-        className="film-card-container"
+        className='film-card-container'
         style={{
           backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${
             listAction.results[this.props.index + 2].poster_path
-          })`,
+          })`
         }}
       />
     ) : (
-      <div></div>
+      <div className='empty-card' />
     );
     return (
-      <div className="all-films-cards">
+      <div className='all-films-cards'>
         {posterPath}
         {posterPath2}
         {posterPath3}

64d34e0b20dca2e39ab23e9a2e626495995ace6c
tmicka23 <mickathp42@gmail.com>
2020-04-27 10:20:14 +0200

    refactor handleValidate

--- src/pages/MatchRoom.js
+++ src/pages/MatchRoom.js
@@ -15,15 +15,11 @@ const initialList = [
     backdrop_path: '/stmYfCUGd8Iy6kAMBr6AmWqx8Bq.jpg',
     original_language: 'en',
     original_title: 'Sonic the Hedgehog',
-    genre_ids: [
-      28,
-      35,
-      878,
-      10751
-    ],
+    genre_ids: [28, 35, 878, 10751],
     title: 'Sonic le film',
     vote_average: 7.5,
-    overview: "L'histoire du hérisson bleu le plus rapide du monde qui arrive sur Terre, sa nouvelle maison. Sonic et son nouveau meilleur ami Tom font équipe pour sauver la planète du diabolique Dr. Robotnik, bien déterminé à régner sur le monde entier.",
+    overview:
+      "L'histoire du hérisson bleu le plus rapide du monde qui arrive sur Terre, sa nouvelle maison. Sonic et son nouveau meilleur ami Tom font équipe pour sauver la planète du diabolique Dr. Robotnik, bien déterminé à régner sur le monde entier.",
     release_date: '2020-02-12'
   },
   {
@@ -36,15 +32,11 @@ const initialList = [
     backdrop_path: '/vw3zNfzvnVNF7nIjpiEgcdznfeC.jpg',
     original_language: 'en',
     original_title: 'Mortal Kombat Legends: Scorpions Revenge',
-    genre_ids: [
-      28,
-      12,
-      16,
-      14
-    ],
+    genre_ids: [28, 12, 16, 14],
     title: "Mortal Kombat Legends: Scorpion's Revenge",
     vote_average: 8.5,
-    overview: "Lord Raiden, protecteur d'Earthrealm, doit rassembler les plus grands combattants de son royaume pour le défendre du démoniaque Shang Tsung dans une bataille ultime.",
+    overview:
+      "Lord Raiden, protecteur d'Earthrealm, doit rassembler les plus grands combattants de son royaume pour le défendre du démoniaque Shang Tsung dans une bataille ultime.",
     release_date: '2020-04-12'
   },
   {
@@ -57,14 +49,11 @@ const initialList = [
     release_date: '2020-01-15',
     original_language: 'en',
     original_title: 'Bad Boys for Life',
-    genre_ids: [
-      53,
-      28,
-      80
-    ],
+    genre_ids: [53, 28, 80],
     backdrop_path: '/upUy2QhMZEmtypPW3PdieKLAHxh.jpg',
     adult: false,
-    overview: "Marcus Burnett est maintenant inspecteur de police. Mike Lowery est, quant à lui, en pleine crise de la quarantaine. Ils s'unissent à nouveau lorsqu'un mercenaire albanais, dont ils ont tué le frère, leur promet une importante prime.",
+    overview:
+      "Marcus Burnett est maintenant inspecteur de police. Mike Lowery est, quant à lui, en pleine crise de la quarantaine. Ils s'unissent à nouveau lorsqu'un mercenaire albanais, dont ils ont tué le frère, leur promet une importante prime.",
     poster_path: '/xbo43M8kdKK5Ku2oaKDMO9PNyo5.jpg'
   },
   {
@@ -77,15 +66,11 @@ const initialList = [
     backdrop_path: '/fYPiQewg7ogbzro2XcCTACSB2KC.jpg',
     original_language: 'en',
     original_title: 'Underwater',
-    genre_ids: [
-      28,
-      27,
-      878,
-      53
-    ],
+    genre_ids: [28, 27, 878, 53],
     title: 'Underwater',
     vote_average: 6.5,
-    overview: "Une équipe scientifique sous-marine fait face à un tremblement de terre. Sous l'eau, ils vont devoir essayer de survivre.",
+    overview:
+      "Une équipe scientifique sous-marine fait face à un tremblement de terre. Sous l'eau, ils vont devoir essayer de survivre.",
     release_date: '2020-01-08'
   }
 ];
@@ -105,22 +90,21 @@ class MatchRoom extends React.Component {
 
   handleValidate = () => {
     const user1List = this.state.user1List.slice();
-    if (this.state.index < this.state.list.length - 1) {
-      user1List.push(this.state.list[this.state.index].title);
-      this.setState({ user1List, index: (this.state.index + 1) });
-    } else if (this.state.index === this.state.list.length - 1) {
-      user1List.push(this.state.list[this.state.index].title);
-      this.setState({ user1List, index: (this.state.index + 1), finishedSession: true });
-    }
-  }
+    user1List.push(this.state.list[this.state.index]);
+    this.setState({
+      user1List,
+      index: this.state.index + 1,
+      finishedSession: this.state.index === this.state.list.length - 1
+    });
+  };
 
   handleReject = () => {
     if (this.state.index < this.state.list.length - 1) {
-      this.setState({ index: (this.state.index + 1) });
+      this.setState({ index: this.state.index + 1 });
     } else {
       return this.setState({ finishedSession: true });
     }
-  }
+  };
 
   handleSession = () => {
     const currentSession = 'user2';
@@ -134,10 +118,16 @@ class MatchRoom extends React.Component {
         <HeaderSmall />
         <Link to='/result'>
           <h2>Result</h2>
-
         </Link>
         {this.state.currentSession === 'user1' ? (
-          <User1List user1={user1} {...this.state} onHandleSession={this.handleSession} onHandleReject={this.handleReject} onHandleValidate={this.handleValidate} user2={user2} />
+          <User1List
+            user1={user1}
+            {...this.state}
+            onHandleSession={this.handleSession}
+            onHandleReject={this.handleReject}
+            onHandleValidate={this.handleValidate}
+            user2={user2}
+          />
         ) : (
           <User2List user2={user2} />
         )}

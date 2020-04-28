2ab35f63f2670fa9fb5c702fa033dd487be9e621
tmicka23 <mickathp42@gmail.com>
2020-04-28 15:39:22 +0200

    semistandard fix

--- src/pages/MatchRoom.js
+++ src/pages/MatchRoom.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import User1List from "../components/User1List";
-import User2List from "../components/User2List";
-import HeaderSmall from "../components/HeaderSmall";
-import ApiKey from "../ApiKey";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import User1List from '../components/User1List';
+import User2List from '../components/User2List';
+import HeaderSmall from '../components/HeaderSmall';
+import ApiKey from '../ApiKey';
 
 class MatchRoom extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       apiList: [],
@@ -14,9 +14,9 @@ class MatchRoom extends React.Component {
       user2List: [],
       index: 0,
       finishedSession: false,
-      currentSession: "user1",
+      currentSession: 'user1',
       listIsLoading: true,
-      fetchListError: false,
+      fetchListError: false
     };
   }
 
@@ -26,19 +26,19 @@ class MatchRoom extends React.Component {
     this.setState({
       user1List,
       index: this.state.index + 1,
-      finishedSession: this.state.index === this.state.apiList.length - 1,
+      finishedSession: this.state.index === this.state.apiList.length - 1
     });
   };
 
   handleReject = () => {
     this.setState({
       index: this.state.index + 1,
-      finishedSession: this.state.index === this.state.apiList.length - 1,
+      finishedSession: this.state.index === this.state.apiList.length - 1
     });
   };
 
   handleSession = () => {
-    const currentSession = "user2";
+    const currentSession = 'user2';
     this.setState({ currentSession });
   };
 
@@ -56,23 +56,23 @@ class MatchRoom extends React.Component {
             this.setState({ apiList: data.results, listIsLoading: false });
           })
           .catch(() => {
-            //console.error('api not responding with the list')
+            // console.error('api not responding with the list')
             this.setState({ listIsLoading: false, fetchListError: true });
           });
       });
   };
 
-  componentDidMount() {
+  componentDidMount () {
     this.getData();
   }
 
-  render() {
+  render () {
     const { user1, user2 } = this.props;
     if (this.state.listIsLoading) {
       return (
         <div>
           <HeaderSmall />
-          <Link to="/result">
+          <Link to='/result'>
             <h2>Result</h2>
           </Link>
           <p>En cours de chargement ...</p>
@@ -82,14 +82,14 @@ class MatchRoom extends React.Component {
       return (
         <div>
           <HeaderSmall />
-          <Link to="/result">
+          <Link to='/result'>
             <h2>Result</h2>
           </Link>
           <p>Erreur lors du chargement</p>
         </div>
       );
     } else {
-      return this.state.currentSession === "user1" ? (
+      return this.state.currentSession === 'user1' ? (
         <User1List
           user1={user1}
           {...this.state}

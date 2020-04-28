f34ccbfcf55af672524b301141239f0550567f77
Geoffroy <geoffroy.crabieres@gmail.com>
2020-04-28 19:19:17 +0200

    fixed CSS on drawer closer element

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -29,77 +29,76 @@ class Drawer extends React.Component {
     const getStars = starsDisplay(voteAverage);
 
     return (
-        <div className='drawer-container'>
-          <div className='drawer-overlay' />
-          <div className="close-drawer"><span/></div>
-          <div className='drawer-movie-all-info-container'>
-              <div
-                className='drawer-movie-banner' style={{
-                  backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${
+      <div className='drawer-container'>
+        <div className='drawer-overlay' />
+        <div className='close-drawer'><span /></div>
+        <div className='drawer-movie-all-info-container'>
+          <div
+            className='drawer-movie-banner' style={{
+              backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${
                         listAction.results[0].poster_path
                     })`
-                }}
-              >
-              </div>
-              <div className='drawer-link-rating'>
-                <a href='#'>
-                  <div className='button'>
-                    <span className='drawer-play-icon' />
+            }}
+          />
+          <div className='drawer-link-rating'>
+            <a href='#'>
+              <div className='button'>
+                <span className='drawer-play-icon' />
                                         Bande annonce
-                  </div>
-                </a>
-                <div className='drawer-rating-container'>
-                  {getStars.map(star => {
-                    if (star === 1) {
-                      return <span className='star full' />;
-                    } else if (star === 0.5) {
-                      return <span className='star half' />;
-                    } else {
-                      return <span className='star empty' />;
-                    }
-                  })}
-                  <span className='global-rating'>{`${voteAverage}/5`}</span>
-                </div>
               </div>
-            <div className='drawer-movie-info-container'>
-              <h4>{listAction.results[0].title}</h4>
-              <span>{`De [realisateur] - [genre] - [durée] - ${listAction.results[0].release_date}`}</span>
-              <p>{listAction.results[0].overview}</p>
+            </a>
+            <div className='drawer-rating-container'>
+              {getStars.map(star => {
+                if (star === 1) {
+                  return <span className='star full' />;
+                } else if (star === 0.5) {
+                  return <span className='star half' />;
+                } else {
+                  return <span className='star empty' />;
+                }
+              })}
+              <span className='global-rating'>{`${voteAverage}/5`}</span>
             </div>
-            <div className='drawer-movie-casting'>
-              <h5>Casting</h5>
-              <div className='drawer-actor-container'>
-                <div className='drawer-actor'>
-                  <img src='#' alt='[actor name]' />
-                </div>
-                <div className='drawer-actor'>
-                  <img src='#' alt='[actor name]' />
-                </div>
-                <div className='drawer-actor'>
-                  <img src='#' alt='[actor name]' />
-                </div>
-                <div className='drawer-actor'>
-                  <img src='#' alt='[actor name]' />
-                </div>
-                <div className='drawer-actor'>
-                  <img src='#' alt='[actor name]' />
-                </div>
-                <div className='drawer-actor'>
-                  <img src='#' alt='[actor name]' />
-                </div>
-                <div className='drawer-actor'>
-                  <img src='#' alt='[actor name]' />
-                </div>
-                <div className='drawer-actor'>
-                  <img src='#' alt='[actor name]' />
-                </div>
-                <div className='drawer-actor'>
-                  <img src='#' alt='[actor name]' />
-                </div>
+          </div>
+          <div className='drawer-movie-info-container'>
+            <h4>{listAction.results[0].title}</h4>
+            <span>{`De [realisateur] - [genre] - [durée] - ${listAction.results[0].release_date}`}</span>
+            <p>{listAction.results[0].overview}</p>
+          </div>
+          <div className='drawer-movie-casting'>
+            <h5>Casting</h5>
+            <div className='drawer-actor-container'>
+              <div className='drawer-actor'>
+                <img src='#' alt='[actor name]' />
+              </div>
+              <div className='drawer-actor'>
+                <img src='#' alt='[actor name]' />
+              </div>
+              <div className='drawer-actor'>
+                <img src='#' alt='[actor name]' />
+              </div>
+              <div className='drawer-actor'>
+                <img src='#' alt='[actor name]' />
+              </div>
+              <div className='drawer-actor'>
+                <img src='#' alt='[actor name]' />
+              </div>
+              <div className='drawer-actor'>
+                <img src='#' alt='[actor name]' />
+              </div>
+              <div className='drawer-actor'>
+                <img src='#' alt='[actor name]' />
+              </div>
+              <div className='drawer-actor'>
+                <img src='#' alt='[actor name]' />
+              </div>
+              <div className='drawer-actor'>
+                <img src='#' alt='[actor name]' />
               </div>
             </div>
           </div>
         </div>
+      </div>
     );
   }
 }

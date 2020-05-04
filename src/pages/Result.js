3e6fe6d66d7dbb6525a027a77a6ae8d6921aa4c4
Geoffroy <geoffroy.crabieres@gmail.com>
2020-05-04 15:18:57 +0200

    added header to result match page

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -17,7 +17,8 @@ class Result extends React.Component {
     return (
       <div className='result'>
         {this.state.matchList.length === 0 ? (
-          <><div className='centered'>
+          <>
+            <div className='centered'>
               <HeaderSmall />
 
               <h2 className='subtitle'>Oh non, vous n’avez aucun match !</h2>
@@ -29,17 +30,20 @@ class Result extends React.Component {
           </>
         ) : (
           <>
-            <h2 className='title'>{this.state.matchList.length === 1 ? 'Bravo, vous avez 1 match !' : `Bravo, vous avez ${this.state.matchList.length} matchs !`}</h2>
-            <div className='matched-movie-container'>
-              {this.state.matchList.map((film) => {
-                return (
-                  <div
-                    className='matched-movie'
-                    style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${film.poster_path})` }}
-                    key={film.id}
-                  />
-                );
-              })}
+            <div className='centered'>
+              <HeaderSmall />
+              <h2 className='title'>{this.state.matchList.length === 1 ? 'Bravo, vous avez 1 match !' : `Bravo, vous avez ${this.state.matchList.length} matchs !`}</h2>
+              <div className='matched-movie-container'>
+                {this.state.matchList.map((film) => {
+                  return (
+                    <div
+                      className='matched-movie'
+                      style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${film.poster_path})` }}
+                      key={film.id}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </>
         )}

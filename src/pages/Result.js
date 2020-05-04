aba0829e0b6be7e90dd338605896f98b965a5aeb
Gottfrieden <60502565+Gottfrieden@users.noreply.github.com>
2020-05-04 18:56:43 +0200

    Feature/us11 (#44)
    * compared user1List and user2List to get matched movies
    
    * updated lodash import
    
    * sent matchroom state to app
    
    * conditional result page + displaying matched movies + CSS
    
    * added header to result match page

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -8,20 +8,45 @@ import '../styles/Result.css';
 class Result extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      matchList: this.props.matchList
+    };
   }
 
   render () {
     return (
       <div className='result'>
-        <div className='centered'>
-          <HeaderSmall />
-          <h2 className='subtitle'>Oh non, vous n’avez aucun match !</h2>
-          <Link to='/matchroom'>
-            <Button content="Continuer d'explorer la liste" className='button' />
-          </Link>
-        </div>
-        <MovieLists />
+        {this.state.matchList.length === 0 ? (
+          <>
+            <div className='centered'>
+              <HeaderSmall />
+
+              <h2 className='subtitle'>Oh non, vous n’avez aucun match !</h2>
+              <Link to='/matchroom'>
+                <Button content="Continuer d'explorer la liste" className='button' />
+              </Link>
+            </div>
+            <MovieLists />
+          </>
+        ) : (
+          <>
+            <div className='centered'>
+              <HeaderSmall />
+              <h2 className='title'>{this.state.matchList.length === 1 ? 'Bravo, vous avez 1 match !' : `Bravo, vous avez ${this.state.matchList.length} matchs !`}</h2>
+              <div className='matched-movie-container'>
+                {this.state.matchList.map((film) => {
+                  return (
+                    <div
+                      className='matched-movie'
+                      style={{ backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)), url(http://image.tmdb.org/t/p/w342/${film.poster_path})` }}
+                      key={film.id}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </>
+        )}
       </div>
     );
   }

a41d114e8618a26d13965d5f2396ae703573a72a
Amélie <ameliechevalier3006@gmail.com>
2020-05-12 11:31:36 +0200

    precise movie in API request

--- src/components/TrendList.js
+++ src/components/TrendList.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Link }  from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../styles/TrendList.css';
 
 const TrendList = () => {
-
-    return(
-        <div className='trendlist-container'>
-            <h3 className='list-category'>Tendances</h3>
-            <Link to='/matchroom/trending/day'><div className='trendlist-day' >Du jour</div></Link>
-            <Link to='/matchroom/trending/week'><div className='trendlist-week'>De la semaine</div></Link>
-        </div>
-    )
-}
+  return (
+    <div className='trendlist-container'>
+      <h3 className='list-category'>Tendances</h3>
+      <Link to='/matchroom/trending/day'><div className='trendlist-day'>Du jour</div></Link>
+      <Link to='/matchroom/trending/week'><div className='trendlist-week'>De la semaine</div></Link>
+    </div>
+  );
+};
 
 export default TrendList;
--- src/pages/MatchRoom.js
+++ src/pages/MatchRoom.js
@@ -197,20 +197,20 @@ class MatchRoom extends React.Component {
           });
         });
     } else if (type === 'trending') {
-      window.fetch(`https://api.themoviedb.org/3/trending/all/${id}?api_key=${ApiKey}`)
-      .then((response) => {
-        return response
-          .json()
-          .then((data) => {
-            this.setState({
-              apiList: data.results,
-              listIsLoading: false
+      window.fetch(`https://api.themoviedb.org/3/trending/movie/${id}?api_key=${ApiKey}`)
+        .then((response) => {
+          return response
+            .json()
+            .then((data) => {
+              this.setState({
+                apiList: data.results,
+                listIsLoading: false
+              });
+            })
+            .catch(() => {
+              this.setState({ listIsLoading: false, fetchListError: true });
             });
-          })
-          .catch(() => {
-            this.setState({ listIsLoading: false, fetchListError: true });
-          });
-      });
+        });
     }
   }
 
